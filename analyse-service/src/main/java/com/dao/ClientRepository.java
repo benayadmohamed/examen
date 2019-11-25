afcac2a97346f8bf490474ad667bd495531accc9
medbenayad <simobend1995@gmail.com>
2019-11-25 17:13:56 +0100

    c1

--- analyse-service/src/main/java/com/dao/ClientRepository.java
+++ analyse-service/src/main/java/com/dao/ClientRepository.java
@@ -8,23 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-//@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.DELETE})
 @RepositoryRestResource(path = "clients")
 public interface ClientRepository extends MongoRepository<Client, String> {
-//    /people/search/nameStartsWith?name=
-//    @RestResource(path = "nameStartsWith", rel = "nameStartsWith")
-//public Page findByNameStartsWith(@Param("name") String name, Pageable p);
-
-/*
-    @Query("Select t from Data t " +
-            "where " +
-            "(:name IS NULL or t.name like :name) AND " +
-            "(:age IS NULL or t.age = :age) AND " +
-            "(:address IS NULL or t.address = :address) AND " +
-            "(:etc IS NULL or t.etc= :etc)")
-    @RestResource(path = "all", rel = "all")
-    Page findByAll(@Param("name") String name, @Param("age") String age,
-                   @Param("address") String address, @Param("etc") String etc, Page page);*/
 
     @RestResource(path = "params", rel = "params")
     public Page findByNomStartsWith(@Param("nom") String nom, Pageable p);
